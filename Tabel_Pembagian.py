f97c19112d68eaddc7a784ab269d0a412c274f8e
Goodwillhakim <goodwillhakim9@gmail.com>
2025-07-09 21:42:44 +0700

    Add files via upload

---
+++ Tabel_Pembagian.py
@@ -0,0 +1,8 @@
+print("=" *41)
+print("             Tabel Pembagian")
+print("=" *41)
+
+for angka in range (1, 11) :
+    for kelipatan in range (1, 11) :
+        print(f" {angka} : {kelipatan} = {angka / kelipatan} ")
+    
